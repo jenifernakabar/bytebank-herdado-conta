01ba9587a2bda0fa8dc7a611adcc65665c5a7740
DJMB <nakabar@gmail.com>
2021-12-30 19:20:24 -0300

    first commit

---
+++ src/Conta.java
@@ -0,0 +1,61 @@
+public abstract class Conta {
+
+	protected double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+
+	
+	public Conta(int agencia, int numero) {
+		this.agencia = agencia;
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public abstract void deposita(double valor);
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
+}
---
+++ src/TesteContas.java
@@ -0,0 +1,19 @@
+
+public class TesteContas {
+
+	public static void main(String[] args) {
+	
+		ContaCorrente cc = new ContaCorrente(111, 111);
+		cc.deposita(100.0);
+
+		ContaPoupanca cp = new ContaPoupanca(222, 222);
+		cp.deposita(100.0);
+
+		cc.transfere(10.0, cp);
+		
+		cc.saca(10.0);
+		System.out.println("CC: " + cc.getSaldo());
+		System.out.println("CP: " + cp.getSaldo());
+	}
+
+}
